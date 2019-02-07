ca538bd6a282f51eb1612e39237480c974fe2f8a
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-02-07 00:56:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,9 +9,18 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
- html = File.read('fixtures/kickstarter.html')
- kickstarter = Nokogiri::HTML(html)
- binding.pry
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+ 
+  projects = {}
+ 
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+ 
+  # return the projects hash
+  projects
 end
 
 create_project_hash
